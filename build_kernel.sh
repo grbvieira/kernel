39e67ad746f2258967e8f9d318273afa0030a5fe
Gerson Vieira <gersonbase@gmail.com>
2023-09-17 02:47:46 -0400

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,39 +1,9 @@
 #!/bin/bash
 
-#export PLATFORM_VERSION=11
-#export ANDROID_MAJOR_VERSION=r
-#export ARCH=arm64
-#export SEC_BUILD_CONF_VENDOR_BUILD_OS=13
-
-#make exynos9830-c2sxxx_defconfig
-#make -j16
-
-
-#!/bin/bash
-
-mkdir out
-
-DTB_DIR=$(pwd)/out/arch/arm64/boot/dts
-mkdir ${DTB_DIR}/exynos
-
 export PLATFORM_VERSION=11
 export ANDROID_MAJOR_VERSION=r
 export ARCH=arm64
 export SEC_BUILD_CONF_VENDOR_BUILD_OS=13
 
-make O=out ARCH=arm64 exynos9830-c2sxxx_defconfig
-
-make O=out ARCH=arm64 -j16
-
-$(pwd)/tools/mkdtimg cfg_create $(pwd)/out/dtb.img dt.configs/exynos9830.cfg -d ${DTB_DIR}/exynos
-
-IMAGE="out/arch/arm64/boot/Image"
-if [[ -f "$IMAGE" ]]; then
-	rm AnyKernel3/zImage > /dev/null 2>&1
-	rm AnyKernel3/dtb > /dev/null 2>&1
-	rm AnyKernel3/*.zip > /dev/null 2>&1
-	mv out/dtb.img AnyKernel3/dtb
-	mv $IMAGE AnyKernel3/zImage
-	cd AnyKernel3
-	zip -r9 Kernel-N986B.zip .
-fi
+make exynos9830-c2sxxx_defconfig
+make -j16
