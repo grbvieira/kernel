2683f2c8db592e82b416d51cb3cb6fdddaa0cd43
Gerson Vieira <gersonbase@gmail.com>
2023-10-01 19:08:19 -0400

    adjust anykernel for note 20 ultra

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=leanKernel by chrisaw @ xda-developers
+kernel.string=Note 20 Ultra 5G Kernel by grbvieira @ xda-developers
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
